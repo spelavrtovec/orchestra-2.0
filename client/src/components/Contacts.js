0c28d99f6b8bec1bf7b78187dcabe6d62e9136ff
spelavrtovec <spela@vrtovec.net>
2018-07-18 13:29:24 +0200

    attempt

--- client/src/components/Contacts.js
+++ client/src/components/Contacts.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-// import { SocialIcon } from "react-social-icons";
+
 
 class Contacts extends Component {
   render() {
@@ -8,18 +8,7 @@ class Contacts extends Component {
         <p className="line1">O r c h e s t r a&emsp;A M A D E O</p>
         <p className="line2">Music and Culture Non-Profit Association</p>
         <img src="" id="logo1" alt="" />
-        {/* <SocialIcon
-          url="https://www.instagram.com/amadeoorchestra/"
-          network="instagram"
-        />
-        <SocialIcon
-          url="https://www.facebook.com/orkesteramadeo/"
-          network="facebook"
-        />
-        <SocialIcon
-          url="https://twitter.com/AmadeoOrchestra"
-          network="twitter"
-        /> */}
+
         <p className="line3">Ljubljana | SLOVENIA</p>
         <p className="line4">
           <span className="one">Tel.</span> +386 31 636 167
