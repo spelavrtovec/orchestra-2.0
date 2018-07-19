9526f502daebf881826c2dd6db7108c284ae6d77
spelavrtovec <spela@vrtovec.net>
2018-07-19 10:43:41 +0200

    some more styleeee

--- client/src/components/Contacts.js
+++ client/src/components/Contacts.js
@@ -1,14 +1,18 @@
 import React, { Component } from "react";
 import { SocialIcon } from "react-social-icons";
-
+import { Image } from "react-bootstrap";
+import z from "../images/z.png";
 
 class Contacts extends Component {
   render() {
     return (
-      <div className="Contacts container">
-        <p className="line1">O r c h e s t r a&emsp;A M A D E O</p>
-        <p className="line2">Music and Culture Non-Profit Association</p>
-        <img src="" id="logo1" alt="" />
+      <div className="Contacts">
+      <Image src={z} className="image-Contacts-one"/>
+      <div className="second-Contacts">
+      <p class="line1">O r c h e s t r a&emsp;A M A D E O</p>
+		<p class="line2">Music and Culture Non-Profit Association</p>
+        <Image src="" id="logo1" alt="" />
+        <div className="social-icons">
         <SocialIcon
           url="https://www.instagram.com/amadeoorchestra/"
           network="instagram"
@@ -21,13 +25,11 @@ class Contacts extends Component {
           url="https://twitter.com/AmadeoOrchestra"
           network="twitter"
         />
-        <p className="line3">Ljubljana | SLOVENIA</p>
-        <p className="line4">
-          <span className="one">Tel.</span> +386 31 636 167
-        </p>
-        <p className="line5">
-          <span className="one">E-mail</span> amadeoorchestra@gmail.com
-        </p>
+        </div>
+        <p class="line3">Ljubljana | SLOVENIA</p>
+		<p class="line4"><span class="one">Tel.</span> +386 31 636 167</p>
+		<p class="line5"><span class="one">E-mail</span> amadeoorchestra@gmail.com</p>
+        </div>
       </div>
     );
   }
