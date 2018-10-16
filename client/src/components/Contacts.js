0202a50648b72f256083272abd1f2450c2f1b648
spelavrtovec <spela@vrtovec.net>
2018-10-16 14:31:14 +0200

    menu works

--- client/src/components/Contacts.js
+++ client/src/components/Contacts.js
@@ -1,15 +1,12 @@
 import React, { Component } from "react";
 import { SocialIcon } from "react-social-icons";
 import { Image } from "react-bootstrap";
-import z from "../images/z.png";
 
 class Contacts extends Component {
   render() {
     return (
       <div className="Contacts">
-      <Image src={z} className="image-Contacts-one"/>
       <div className="second-Contacts">
-      <p className="line1">O r c h e s t r a&emsp;A M A D E O</p>
 		  <p className="line2">Music and Culture Non-Profit Association</p>
         <Image src="" id="logo1" alt="" />
         <div className="social-icons">
